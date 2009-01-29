0b7fe39ab579fb7f31e1f6d32d7f75b13f56fadf
csaftoiu <Claudiu_Saftoiu@Brown.edu>
2009-01-29 17:32:07 -0500

    Made ./run print the pretty printer output

--- src/Tjs.hs
+++ src/Tjs.hs
@@ -4,7 +4,17 @@ import TypedJavaScript.Syntax
 import TypedJavaScript.Parser
 import TypedJavaScript.Lexer
 
+import Text.PrettyPrint.HughesPJ ( render, vcat )
+import TypedJavaScript.PrettyPrint (pp)
+
+pretty :: [ParsedStatement] -> String
+pretty stmts = render $ vcat $ map pp stmts
+
 main = do
   strscript <- getContents
   putStrLn $ show $ parseString strscript
+  putStrLn ""
+  putStrLn "Pretty-printed version:"
+  putStrLn "-----------------------"
+  putStrLn $ pretty $ parseString strscript
 
