f2a7a78c4345c78025aa02f6a7b6cc4233da02d7
Arjun Guha <arjun@cs.brown.edu>
2009-05-15 07:53:08 -0400

    comment fix by Claudiu a long time ago

--- src/TypedJavaScript/Syntax.hs
+++ src/TypedJavaScript/Syntax.hs
@@ -35,7 +35,7 @@ data Type
   | TRec String Type
   | TSequence [Type] (Maybe Type) -- sequence of types (e.g. arguments array)
   | TFunc Type --arg types. should be:
-               --  rec args. SEQUENCE[thistype, args, realargs]
+               --  SEQUENCE[thistype, argsarray realargs]
           Type {- ret type -}
           LatentPred {- latent predicate -} 
   | TId String -- an Id defined through a 'type' statement
