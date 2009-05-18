f0fca6e188d4e924123e2f264650b9ee148ac123
Arjun Guha <arjun@cs.brown.edu>
2009-05-18 16:33:24 -0400

    soundness2 e.g

--- tests/type-check/soundness2.js
+++ tests/type-check/soundness2.js
@@ -15,6 +15,16 @@ function() :: (->) {
   y.x = {field: 30}; //this should fail, because now z.x.field2 is broken.
 } @@ fails;
 
+function () :: (->) {
+  function oomra(y) :: ({x :: {field :: int}} ->) {
+    y.x = {field: 30};
+  }
+
+  var z = {x : {field:50, field2: 9000}};
+  oomra(z);
+  z.x.field2;
+} @@ fails;
+
 function() :: (->) {
   function writeToF2(bar) :: ({field::int, field2::int} -> ) {
     bar.field2 = 50;
