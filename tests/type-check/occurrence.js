ac877de0b83258ce61e12d2c96549ae5ccc698b5
csaftoiu <Claudiu_Saftoiu@Brown.edu>
2009-05-29 17:53:15 -0400

    fixed occurrence cases with &&

--- tests/type-check/occurrence.js
+++ tests/type-check/occurrence.js
@@ -479,6 +479,9 @@ function (x) :: (U(int->bool, bool->int) -> bool) {
 } :: (U(int->bool, bool->int) -> bool);*/
 
 //&&
+function (dubfun, x) :: ((double -> bool), U(double,string) -> bool) {
+  return (typeof(x)=='number' && isNaN(x)) || false;
+} :: ((double -> bool), U(double, string) -> bool);
 function (dubfun, x) :: ((double -> bool), any -> bool) {
-  return (typeof(dubfun)=='number' && isNaN(x)) || false;
+  return (typeof(x)=='number' && isNaN(x)) || false;
 } :: ((double -> bool), any -> bool);
