e4a3bc6a84b36a45f867eeba3255f27e4e169205
Arjun Guha <arjun@cs.brown.edu>
2009-08-06 10:25:07 -0400

    configuration

--- Setup.hs
+++ Setup.hs
@@ -1,13 +1,4 @@
 #!/usr/bin/env runhaskell
 import Distribution.Simple
-import System.Process (runCommand,waitForProcess)
 
-testMain args _ _ _ = do
-  let cmd = "TypedJavaScript_datadir=\"data\" \
-            \./dist/build/typedjs-tests/typedjs-tests"
-  handle <- runCommand cmd
-  waitForProcess handle
-  putStrLn "Testing complete.  Errors reported above (if any)."
-
-
-main = defaultMainWithHooks (simpleUserHooks { runTests = testMain })
+main = defaultMain
